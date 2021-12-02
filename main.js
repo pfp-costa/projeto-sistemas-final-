48a51efe0641f4dc2332a90ece8c5935c402ce53
pfp-costa <87148657+pfp-costa@users.noreply.github.com>
2021-12-02 13:36:47 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+// abre e fecha //
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+//quando clicar no menu esconder//
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+// MUDAR O HEADER COM SCROLL//
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
